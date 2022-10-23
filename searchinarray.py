0d645f375e942826fcaaaa6d3d8f0f89acf395f3
samratkarak10 <114635510+samratkarak10@users.noreply.github.com>
2022-10-23 18:41:45 +0530

    Create searchinarray.py

---
+++ searchinarray.py
@@ -0,0 +1,9 @@
+arr = [1, 2, 3, 4, 5, 6, 7, 8]
+searchValue = 20
+
+if(searchValue in arr):
+   index = arr.index(searchValue)
+   print("The element", searchValue, " is found at: ", index)
+else:
+   print("The element you are searching for here is not present in the array")
+    # thank you from samrat
